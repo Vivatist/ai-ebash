38d6ca1f3c10264ccc792166a26c879cf2f4b50d
Andrey BCH <andrey.bch.1976@gmail.com>
2025-09-14 18:26:34 +0300

    Новый конфигуратор и возможность работать с любыми нейронками

--- src/aiebash/llm_factory.py
+++ src/aiebash/llm_factory.py
@@ -1,6 +1,7 @@
 # llm_factory.py
 from .llm_interface import LLMClient
 from .openai_client import OpenAIClientOverProxy, OpenAIClient
+from .default_client import DefaultClient
 # сюда в будущем можно добавить HuggingFaceClient, OllamaClient и др.
 
 def create_llm_client(backend: str, model: str, api_url: str, api_key: str = None, timeout: int = 60) -> LLMClient:
@@ -12,4 +13,7 @@ def create_llm_client(backend: str, model: str, api_url: str, api_key: str = Non
     # elif backend == "ollama":
     #     return OllamaClient(...)
     else:
-        raise ValueError(f"Неизвестный backend: {backend}")
+        try:
+            return DefaultClient(model=model, api_url=api_url, api_key=api_key, timeout=timeout)
+        except :
+            raise ValueError(f"Не удается создать LLM клиент для: {backend}")
